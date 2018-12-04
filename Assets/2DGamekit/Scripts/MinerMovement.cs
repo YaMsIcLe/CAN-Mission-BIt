1bc13b8518d963395aa80b09c7f2710cf91a1360
Andrew Vuong <anvuong3@s.sfusd.edu>
2018-12-03 16:44:10 -0800

    Andrew's New Edits

---
+++ Assets/2DGamekit/Scripts/MinerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Gamekit2D
+{
+    public class MinerMovement : MonoBehaviour
+    {
+        public bool test;
+        private bool facingRight = true;
+        public float maxSpeed = 10;
+        private Rigidbody2D rb;
+        private Animator animator;
+        private PlayerInput pInput;
+        private bool jump;
+        public int numJumps = 1;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            pInput = GetComponent<PlayerInput>();
+        }
+
+        private void Update()
+        {
+            if (rb.velocity.x > 0 && !facingRight)
+            {
+                Flip();
+            }
+            else if (rb.velocity.x < 0 && facingRight)
+            {
+                Flip();
+            }
+
+            if (rb.velocity.y > 0)
+            {
+                numJumps = 0;
+            }
+            else if (rb.velocity.y == 0)
+            {
+                numJumps = 1;
+            }
+
+            if (pInput.Jump.Down && numJumps > 0)
+            {
+                jump = true;
+            }
+
+            animator.SetBool("isWalking", rb.velocity.x != 0);
+            animator.SetBool("isJumping", rb.velocity.y != 0);
+        }
+
+        private void FixedUpdate()
+        {
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * maxSpeed, rb.velocity.y);
+
+            if (jump)
+            {
+                rb.AddForce(new Vector2(0, 1000));
+                numJumps--;
+                jump = false;
+            }
+        }
+
+        private void Jump()
+        {
+            Debug.Log(Input.GetAxis("Space"));
+        }
+
+        private void Flip()
+        {
+            facingRight = !facingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
+
---
+++ Assets/2DGamekit/Scripts/MyCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    public bool test;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
